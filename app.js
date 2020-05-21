f947ec686ea242bd660ee4d6651714ccff281a0e
Katrin Meyfarth <Katrin@Katrins-MacBook-Pro.local>
2020-05-21 19:44:49 +0100

    Test Elasic Beanstalk Github pipeline deploy

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send("Welcome to the home page v1.1");
+    res.send("Welcome to the home page v1.2");
 });
 
 const port = process.env.port || '3000';
